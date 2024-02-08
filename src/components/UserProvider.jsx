9238725691155ba99e15622f4801afe1cd4e0beb
cristinaarose <cristinarosehart@gmail.com>
2024-02-08 16:40:27 +0000

    add delete comment functionality

--- src/components/UserProvider.jsx
+++ src/components/UserProvider.jsx
@@ -5,8 +5,8 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [loggedInUser, setLoggedInUser] = useState({
-    username: "Cristina_H",
-    name: "Cristina Hart",
+    username: "jessjelly",
+    name: "Jess Jelly",
     avatar_url:
       "https://vignette.wikia.nocookie.net/mrmen/images/4/4f/MR_JELLY_4A.jpg/revision/latest?cb=20180104121141",
   });
